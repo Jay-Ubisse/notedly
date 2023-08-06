b292e1ee5f61fe9f04dc436251b093624192b83a
Jay-Ubisse <joaquimubisse@gmail.com>
2023-08-06 23:37:09 +0200

    apollo-server setup

--- api/src/index.js
+++ api/src/index.js
@@ -1,2 +1,33 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const port = process.env.PORT || 4000;
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Ola, Mundo!',
+  },
+};
+
+const app = express();
+async function startServer() {
+  apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app, path: '/api' });
+}
+startServer();
+
+app.listen({ port }, () =>
+  console.log(`🚀 Server ready athttp://localhost:${port}/api`)
+);
